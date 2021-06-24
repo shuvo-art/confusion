528d0f8d1f2a77d721a647d5c3ee0c1dbe934773
Hassan Shuvo <shuvo83qn@gmail.com>
2021-06-24 12:00:21 +0600

    Redux Thunk and Redux Logger

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => (val) && (val.length >= len);
 
     function RenderDish({dish}) {
         return(
@@ -43,9 +48,6 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
         }
     }
 
-    const required = (val) => val && val.length;
-    const maxLength = (len) => (val) => !(val) || (val.length <= len);
-    const minLength = (len) => (val) => (val) && (val.length >= len);
 
     class CommentForm extends Component{
 
@@ -141,8 +143,25 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
     }
 
     const DishDetail = (props) => {
-        
-        if (props.dish != null) {
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) {
             return(
                 <div className="container">
                         <div className="row">
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb,BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
     function RenderMunuItem({ dish, onClick}) {
       return(
@@ -16,7 +17,7 @@ import { Link } from 'react-router-dom';
     }
 
     const Menu = (props) => {
-        const menu = props.dishes.map((dish) => {  
+        const menu = props.dishes.dishes.map((dish) => {  
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
                   <RenderMunuItem dish={dish} />
@@ -24,8 +25,26 @@ import { Link } from 'react-router-dom';
             );
         });
 
-
-        return (
+        if (props.dishes.isLoading) {
+              return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dishes.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.dishes.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else{
+          return (
             <div className="container">
                 <div className="row">
                   <Breadcrumb>
@@ -42,6 +61,7 @@ import { Link } from 'react-router-dom';
                 </div>
             </div>
           );
+        }
 
     }
 
